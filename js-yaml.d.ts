683865faffad61c0ab45cff1c15ffae1a86f4338
moduqishi <2429239979@qq.com>
2025-12-09 15:00:10 +0800

    删除了一些不必要的文件

--- js-yaml.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'js-yaml' {
-    export function load(str: string, options?: any): any;
-    export function dump(obj: any, options?: any): string;
-    const _default: {
-        load: typeof load;
-        dump: typeof dump;
-    };
-    export default _default;
-}
